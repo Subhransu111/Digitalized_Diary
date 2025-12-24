bc3e4153b5ae9ce078f7e238ea0c537a6b83cf48
Subhransu <subhransutripathy05@gmail.com>
2025-12-24 14:16:25 +0530

    Security Feature Updated

--- controllers/evidenceLogController.js
+++ controllers/evidenceLogController.js
@@ -1,11 +1,51 @@
 const EvidenceLog = require('../models/Evidence_log');
 const QrCode = require('qrcode');
 const fs = require('fs');
-const { time } = require('console');
+
+//Validate the content of the file
+const validateFileSignature = (filePath)=>{
+    return new Promise((resolve, reject)=>{
+        const stream = fs.createReadStream(filePath,{start:0 , end: 7});
+        let bytes =[]
+        stream.on('data', (chunk)=>{
+            bytes.push(...chunk);
+        });
+        stream.on('end',()=>{
+            const header = bytes.map(b=>b.toString(16).toUpperCase().padStart(2,'0')).join(' ');
+            const isPng = header.startsWith('89504E47');
+            const isPdf = header.startsWith('25504446');
+            const isJpeg = header.startsWith('FFD8FF');
+
+            if(isPng || isPdf || isJpeg){
+                resolve(true);
+            }
+            else{
+                resolve(false);
+            }
+        });
+        stream.on('error',(err)=>{
+            reject(err);
+        }
+        );
+    })
+}
+
 // Create a new Evidence Log
 async function createEvidenceLog(req,res){
     try{
         const {caseId , description, evidenceType, loggedBy} = req.body;
+
+        // Validate file signature if file is uploaded
+        if (req.file){
+            const isSafe = await validateFileSignature(req.file.path);
+            if(!isSafe){
+                fs.unlinkSync(req.file.path);
+                return res.status(400).json({
+                    success:false,
+                    message:"Security Alert: Uploaded file type is not allowed.",
+                });
+            }
+        }
 
         // Validate required fields
         if(!caseId || !description || !evidenceType){
@@ -44,6 +84,7 @@ async function createEvidenceLog(req,res){
         }) ;
     }
     catch(error){
+        if (req.file && fs.existsSync(req.file.path)) fs.unlinkSync(req.file.path);
         return res.status(500).json({
             success:false,
             error:error.message,
