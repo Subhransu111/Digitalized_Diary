8807995cc4e8fea752c585347aae1772a72a6ce0
Subhransu <subhransutripathy05@gmail.com>
2025-12-27 11:58:07 +0530

    final update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
-require('dotenv').config(); // Load environment variables
 const cors = require('cors');
 
 // Import Routes
